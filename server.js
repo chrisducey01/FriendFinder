49ac9436b7a7db1cbaabe2d552cd1c0f9cfe7560
Christopher Ducey <chrisducey01@me.com>
2019-10-23 23:40:32 -0700

    Setup shell for routing for api and html routes

--- server.js
+++ server.js
@@ -1,5 +1,31 @@
+require("dotenv").config();
+
 const express = require("express");
 const mysql = require("mysql");
 const path = require("path");
+const app = express();
 
-require("dotenv").config();
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+const connection;
+if(process.env.JAWSDB_URL){
+    //setup connection to use JAWSDB instance on heroku
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else{
+    //setup local connection
+    connection = mysql.createConnection({
+        user: process.env.LOCAL_DB_USER,
+        password: process.env.LOCAL_DB_PASSWORD,
+        port: process.env.LOCAL_DB_PORT,
+        host: localhost,
+        database: process.env.LOCAL_DB_NAME
+    });
+}
+
+
