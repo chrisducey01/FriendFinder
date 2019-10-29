e0776af4784c5ea5d7bc81f792c5e5f2bf677a13
Christopher Ducey <chrisducey01@me.com>
2019-10-28 22:08:29 -0700

    Changed logic so there are 10 static questions for each user

--- app/db/schema.sql
+++ app/db/schema.sql
@@ -1,18 +1,18 @@
-DROP TABLE IF EXISTS answers;
 DROP TABLE IF EXISTS users;
 
 CREATE TABLE users(
     user_id INT NOT NULL AUTO_INCREMENT,
     name varchar(30) NOT NULL,
     photo_url varchar(256),
+    question_1 INT NOT NULL,
+    question_2 INT NOT NULL,
+    question_3 INT NOT NULL,
+    question_4 INT NOT NULL,
+    question_5 INT NOT NULL,
+    question_6 INT NOT NULL,
+    question_7 INT NOT NULL,
+    question_8 INT NOT NULL,
+    question_9 INT NOT NULL,
+    question_10 INT NOT NULL,
     PRIMARY KEY(user_id)
-);
-
-CREATE TABLE answers(
-    answer_id INT NOT NULL AUTO_INCREMENT,
-    user_id INT NOT NULL,
-    answer_num INT NOT NULL,
-    score INT NOT NULL,
-    PRIMARY KEY(answer_id),
-    FOREIGN KEY(user_id) REFERENCES users(user_id)
 );
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,39 +4,59 @@ module.exports = function (app, connection) {
         // res.json(friendArr);
     });
 
+
     app.post("/api/friends", (req, res) => {
         const person = req.body;
 
-        connection.query("INSERT INTO users(name, photo_url) VALUES (?,?)", [person.name, person.photo_url], (err, data, fields) => {
+        // Build array of all answers from the body object
+        let newAnswerArr = [];
+        for (let i = 1; i <= 10; i++) {
+            let tempAnswer = Number(person[`question_${i}`]);
+            console.log(tempAnswer);
+            console.log(i);
+            newAnswerArr.push(tempAnswer ? tempAnswer : 0);
+        }
+
+        // Get all existing users to determine best match
+        connection.query("SELECT * FROM users", (err, data) => {
             if (err) {
-                console.log(`Error inserting new user into db - user: ${person.name}`);
+                console.log("Error getting friend list from db.");
                 console.log(err);
                 return res.status(500).end();
             }
 
-            let userId = data.insertId;
-            console.log(`New user successfully inserted into db.  New user id: ${userId}`);
+            // Calculate the difference in answers for each existing user
+            let minVal = 999;
+            let minId = -1;
+            let diffArr =
+                data.map(dbPerson => {
+                    let difference = 0;
+                    for (let i = 0; i < 10; i++) {
+                        difference += Math.abs(newAnswerArr[i] - Number(dbPerson[`question_${i + 1}`]));
+                    }
+                    // Keep track of the person that has the closest answers to you
+                    if (difference < minVal) {
+                        minVal = difference;
+                        minId = dbPerson.user_id;
+                    }
+                    dbPerson["difference"] = difference;
+                    return dbPerson;
+                });
 
-            // Build array of answers to insert into DB, attach the new user id as part of the array (foreign key)
-            let answerKeys = Object.keys(req.body).filter(answer => { return answer.startsWith("question") });
-            let answerArr = [];
-            answerKeys.forEach(answerKey => {
-                answerArr.push([ userId, Number(answerKey.split("-")[1]), Number(person[answerKey])]);
-            });
-
-            console.log(answerArr);
-
-            connection.query("INSERT INTO answers (user_id, answer_num, score) VALUES ?",[answerArr],(err,data,fields)=>{
-                if(err){
-                    console.log(`Error inserting answers into db - user_id: ${userId}`);
+            // Insert as new user into db
+            connection.query("INSERT INTO users SET ?", [person], (err, data, fields) => {
+                if (err) {
+                    console.log(`Error inserting new user into db - user: ${person.name}`);
                     console.log(err);
                     return res.status(500).end();
                 }
 
-                console.log(`Answers successfully inserted into db for user id: ${userId}`);
+                let userId = data.insertId;
+                console.log(`New user successfully inserted into db.  New user id: ${userId}`);
 
-                res.json({ user_id: userId });
-            }); //insert answers for user
-        }); //insert user
+                // All transactions are successful, return the best match as a JSON object
+                res.json(diffArr.find(person => { return minId == person.user_id }));
+            }); //insert new user
+        }); //select all existing users
     });  //app.post
 }; //module.exports
